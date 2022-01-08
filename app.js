fb05ccbd15926fd5a611d04a4f7467bf8ec0ca87
terryszhou <terryszhou@gmail.com>
2022-01-08 16:30:59 -0600

    Async fetch function added.

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    getUser() {
-      this.firstName = 'Sam'
-      this.lastName = "Smith"
-      this.email = "sam@gmail.com"
-      this.gender = "female"
-      this.picture = "https://randomuser.me/api/portraits/women/10.jpg"
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      this.firstName = results[0].name.first
+      this.lastName = results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
     },
   },
 });
