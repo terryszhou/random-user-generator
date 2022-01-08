c534333aec257babb9f260470c4ffe1816476463
terryszhou <terryszhou@gmail.com>
2022-01-08 16:27:56 -0600

    Through v-binding function methods.

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const app = Vue.createApp({
       email: "john@gmail.com",
       gender: "male",
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
-    }
+    };
+  },
+  methods: {
+    getUser() {
+      this.firstName = 'Sam'
+      this.lastName = "Smith"
+      this.email = "sam@gmail.com"
+      this.gender = "female"
+      this.picture = "https://randomuser.me/api/portraits/women/10.jpg"
+    },
   },
 });
 
